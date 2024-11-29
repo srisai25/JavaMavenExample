fec394d133e3a1d0292bb6ea8e29ce979ffde3ee
srisai25 <srisaisagarla@gmail.com>
2024-11-29 11:01:59 +0530

    Sample Maven java

---
+++ src/main/java/com/app/JavaMave/JavaMavenExample/App.java
@@ -0,0 +1,10 @@
+package com.app.JavaMave.JavaMavenExample;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("22BD1A055K-SRISAI");
+    }
+}
